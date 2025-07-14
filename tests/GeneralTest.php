6fa733c25c711bc19b743ddd98cfe648be2f0a53
omer <omernaim8@gmail.com>
2025-07-14 08:15:21 +0300

    db

---
+++ tests/GeneralTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class GeneralTest extends TestCase
+{
+
+    protected function setUp(): void
+    {
+
+    }
+
+    protected function tearDown(): void
+    {
+    }
+
+    public function testOne()
+    {
+        $this->assertEquals(1,1);
+    }
+
+}
